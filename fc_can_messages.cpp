3406de9b06ec780dd276eda8db0d3d8ab09628af
Reegan McAllister <reegan@ualberta.ca>
2016-02-19 17:41:19 -0700

    some fuel cell messages are now sending over can

--- fc_can_messages.cpp
+++ fc_can_messages.cpp
@@ -2,7 +2,8 @@
 #include "can_message.h"
 #include "can_message_def.h"
 #include "fc_can_messages.h"
-
+#include <mcp2515_lib.h>
+#include <arduino.h>
 
 /*
 * Create CAN Message Descriptors
@@ -36,4 +37,65 @@ can_msg::MsgEncode fc_total_charge_message( can_msg::UINT32, can_msg::FUEL_CELL,
 //FCVOLT
 can_msg::MsgEncode fcvolt_message( can_msg::INT32, can_msg::FUEL_CELL, can_msg::FCVOLT, can_msg::INFORMATION, 1);
 
-//
+
+
+
+/*
+* Create functions for sending the messages
+*/
+
+
+//FC_ERROR
+void send_fc_error(uint16_t val)
+{
+	//make a messages
+	CanMessage msg;
+	msg.id = fc_error_msg.id();
+	msg.length = fc_error_msg.len();
+	fc_error_msg.buf(msg.data, val);
+	can_send_message(&msg);
+}
+
+//FC_STATE
+void send_fc_state(uint8_t val)
+{
+	//make a messages
+	CanMessage msg;
+	msg.id = fc_state_msg.id();
+	msg.length = fc_state_msg.len();
+	fc_state_msg.buf(msg.data, val);
+	can_send_message(&msg);
+}
+
+//FC_PURGE_COUNT
+void send_fc_purge_count(uint8_t val)
+{
+	//make a messages
+	CanMessage msg;
+	msg.id = fc_purge_count_msg.id();
+	msg.length = fc_purge_count_msg.len();
+	fc_purge_count_msg.buf(msg.data, val);
+	can_send_message(&msg);
+}
+
+//FC_TIME_BETWEEN_LAST_PURGES
+void send_fc_time_between_last_purges(unsigned long val)
+{
+	//make a messages
+	CanMessage msg;
+	msg.id = fc_time_between_last_purges_msg.id();
+	msg.length = fc_time_between_last_purges_msg.len();
+	fc_time_between_last_purges_msg.buf(msg.data, val);
+	can_send_message(&msg);
+}
+
+//FC_ENERGY_SINCE_LAST_PURGE
+void send_fc_energy_since_last_purge(unsigned long val)
+{
+	//make a messages
+	CanMessage msg;
+	msg.id = fc_energy_since_last_purge_msg.id();
+	msg.length = fc_energy_since_last_purge_msg.len();
+	fc_energy_since_last_purge_msg.buf(msg.data, val);
+	can_send_message(&msg);
+}
--- fc_can_messages.h
+++ fc_can_messages.h
@@ -1,8 +1,15 @@
 #ifndef FC_CAN_MESSAGES_H
 #define FC_CAN_MESSAGES_H
 
+void send_fc_error(unsigned int val);
 
+void send_fc_state(unsigned char val);
 
+void send_fc_purge_count(unsigned char val);
+
+void send_fc_time_between_last_purges(unsigned long val);
+
+void send_fc_energy_since_last_purge(unsigned long val);
 
 
 #endif
