c80fd0aeb1139cb0258ef5ae3bdf071858632579
Reegan McAllister <reegan@ualberta.ca>
2016-03-06 13:38:09 -0700

    Compiler was hating me so I moved some stuff around

--- lcd_can_messages.h
+++
@@ -1,9 +0,0 @@
-#ifndef LCD_CAN_MESSAGES_H
-#define LCD_CAN_MESSAGES_H
-
-void send_lcd_horn(bool horn);
-void send_lcd_wipers(bool wipers);
-void send_lcd_signals(bool left, bool right, bool hazards);
-void send_lcd_headlights(bool state);
-
-#endif
